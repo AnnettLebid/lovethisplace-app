d79b0a0bf69b2e695fab59580f6c79dca8ea7dc8
Anna Lebid <annalez16@gmail.com>
2021-06-02 22:22:46 +0300

    add get and create post

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
-import { getPosts } from '..controllers/posts.js'
+import { getPosts, createPost } from '../controllers/posts.js'
 
 const router = express.Router();
 
 router.get('/', getPosts);
+router.get('/', createPost);
+// router.get('/', updatePost);
 
 export default router;
