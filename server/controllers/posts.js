4fd00c9fe2f0e280de65883600bea657fc461627
Anna Lebid <annalez16@gmail.com>
2021-06-06 16:24:35 +0300

    add mongoose dependency

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -27,7 +27,7 @@ export const createPost = async (req, res) => {
 }
 
 export const updatePost = async (req, res) => {
-   const { id } = req.params;
+  const { id } = req.params;
   const _id = id;
   
   const post = req.body;
