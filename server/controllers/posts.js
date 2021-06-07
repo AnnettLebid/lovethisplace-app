fb9118ffd585ffe2fe0759cd718694f55150334f
Anna Lebid <annalez16@gmail.com>
2021-06-07 14:58:27 +0300

    fix likePost

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -52,8 +52,6 @@ export const deletePost = async (req, res) => {
 export const likePost = async (req, res) => {
   const { id } = req.params;
   
-  const post = req.body;
-
   if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
 
   const post = await PostMessage.findById(id);
