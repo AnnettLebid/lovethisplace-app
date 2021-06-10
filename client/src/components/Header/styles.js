81eaa66ef610e5fdb6b69e4badddaa7a225cdffa
Anna Lebid <annalez16@gmail.com>
2021-06-10 11:08:53 +0300

    change transparency from 0.5 to 0.3

--- client/src/components/Header/styles.js
+++ client/src/components/Header/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
   heroContainer: {
-    backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), 
+    backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), 
     url('https://images.unsplash.com/photo-1510414842594-a61c69b5ae57?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80')`,
     // url('https://images.unsplash.com/photo-1621841058209-dcb875c9b970?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1086&q=80')`,
     backgroundSize: 'cover',
