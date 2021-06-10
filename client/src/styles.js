854b48222aa8f0ea07fec60825b8a50b27ab1800
Anna Lebid <annalez16@gmail.com>
2021-06-10 11:10:55 +0300

    delete unused css

--- client/src/styles.js
+++ client/src/styles.js
@@ -1,9 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-  heading: {
-    color: 'rgba(0,183,255, 1)',
-  },
 
   [theme.breakpoints.down('sm')]: {
     mainContainer: {
