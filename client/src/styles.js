7e1f8df10bbb5b6dac77c12e8961adfb98d6f10f
Anna Lebid <annalez16@gmail.com>
2021-06-21 10:51:48 +0300

    move styling of background from hero to Box in the App

--- client/src/styles.js
+++ client/src/styles.js
@@ -1,4 +1,5 @@
 import { makeStyles } from '@material-ui/core/styles';
+import Background from './images/hero-unsplash.jpg';
 
 export default makeStyles((theme) => ({
 
@@ -6,5 +7,19 @@ export default makeStyles((theme) => ({
     mainContainer: {
       flexDirection: "column-reverse",
     }
-  }
+  },
+  container: {   
+    backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${Background})`,       
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    width:"100%",
+    height: "400px",
+    position: "relative",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    // alignItems: "left",    
+    marginBottom: "2rem",
+  }, 
 }));
