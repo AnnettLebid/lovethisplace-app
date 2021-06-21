a6c4240e6010e1b8a88cc3f45873eb8921db8d06
Anna Lebid <annalez16@gmail.com>
2021-06-21 10:57:07 +0300

    remove from Hero styles background

--- client/src/components/Header/styles.js
+++ client/src/components/Header/styles.js
@@ -1,20 +1,14 @@
 import { makeStyles } from '@material-ui/core/styles';
-import Background from '../../images/hero-unsplash.jpg';
 
 export default makeStyles(() => ({
-  heroContainer: {   
-    backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${Background})`,       
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    width:"100%",
-    height: "400px",
+  heroContainer: {     
     position: "relative",
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
-    alignItems: "left",    
-    marginBottom: "2rem",
+    alignItems: "left",   
+    padding: "5rem 0",
+    marginBottom: "4rem",
   },  
   hero: {
     color: "#fff",   
