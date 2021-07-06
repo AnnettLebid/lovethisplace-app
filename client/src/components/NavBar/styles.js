2a2407665b9901f5829aa10426bd7907544fb1c2
Anna Lebid <annalez16@gmail.com>
2021-07-06 13:38:33 +0300

    add styling to navbar

--- client/src/components/NavBar/styles.js
+++ client/src/components/NavBar/styles.js
@@ -1,13 +1,19 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,    
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
+  grow: {
     flexGrow: 1,
+  }, 
+  sectionAuth: {   
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+  },
+  loginButton: {
+    marginLeft: theme.spacing(2),
+  },
+  title: {   
+    color: 'inherit',
+    margin: '1rem',
   },
 }));
