ded37804128ef9954ec145b4963ea4e276886ee7
Anna Lebid <annalez16@gmail.com>
2021-07-13 13:09:26 +0300

    fix warning change createMuiTheme to createTheme

--- client/src/theme.js
+++ client/src/theme.js
@@ -1,9 +1,9 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
 import blue from '@material-ui/core/colors/blue';
 import pink from '@material-ui/core/colors/pink';
 
 // A custom theme for this app
-const theme = createMuiTheme({
+const theme = createTheme({
   palette: {
     primary: blue,
     secondary: pink,
