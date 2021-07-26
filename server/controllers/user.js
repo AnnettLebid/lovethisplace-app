162089a9bce39dc9380bf264be151245af63758e
Anna Lebid <annalez16@gmail.com>
2021-07-26 14:23:05 +0300

    fix typos

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -24,21 +24,21 @@ export const signin = async (req, res) => {
   }
 }
 
-export const signup = async (req, res) => {
+export const signup = async (req, res) => { 
   const { email, password, confirmPassword, firstName, lastName } = req.body;
 
-  try {
+  try {    
     existingUser = await User.findOne({ email });
-
+    
     if (existingUser) { return res.status(400).json({ message: "User already exists!" })};
 
     if(password !== confirmPassword) { return res.status(400).json({ message: "Passwords don't match!" })};
 
     const hashedPassword = await bcrypt.hash(password, 12);
-
-    const result = await User.createConnection({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
-
-    const token = jwt.sign({ email: result.email, id: resilt._id }, 'test', {expiresIn: "1h"});
+   
+    const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
+    
+    const token = jwt.sign({ email: result.email, id: result._id }, 'test', {expiresIn: "1h"});
 
     res.status(200).json({ result, token });
 
