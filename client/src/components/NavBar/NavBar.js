61846035501e1dca644ca69877aaf7d77def5e48
Anna Lebid <annalez16@gmail.com>
2021-07-30 09:24:31 +0300

    update userName for sign in with Google

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -53,7 +53,7 @@ const NavBar = () => {
                 >
                   <AccountCircle />
                 </IconButton>
-                <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+                <Typography className={classes.userName} variant="h6">{`${user.profileData.givenName} ${user.profileData.familyName}`}</Typography>
                 <div>
                   <Button className={classes.loginButton} component={Link} to="/auth" variant="outlined" color="inherit"
                   onClick={logOut}>Logout</Button>
