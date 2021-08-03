bd5eae2951cb62f83073c5cfb4cb018b990beabb
Anna Lebid <annalez16@gmail.com>
2021-08-03 14:55:12 +0300

    fix token retrive from local storage

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,8 +5,8 @@ import axios from 'axios';
 const API = axios.create({ baseURL: `http://localhost:5000` });
 
 API.interceptors.request.use((req) => {
-  if(localStorage.getItem('profile')) {
-    req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('profile').token)}`;
+  if(localStorage.getItem('profile')) {       
+    req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token }`;
   }
   return req;
 });
